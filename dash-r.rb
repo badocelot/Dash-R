967bc129207be1c06b31bf8be5a9bead20b8db94
James M. Jensen II <badocelot@badocelot.com>
2011-05-24 00:59:58 -0500

    Non-numeric revisions now pass through as strings.
    Hopefully this allows branch names and tags to be used.

--- dash-r.rb
+++ dash-r.rb
@@ -4,26 +4,48 @@
 # Copyright 2011  James M. Jensen II <badocelot@badocelot.com>
 # License: GNU General Public License, Version 2 or later
 
-revs = `git log --pretty=oneline`.split("\n").map { |commit| commit.split[0] }
-
-if (ARGV.length == 0) then
-	revs.length.times do |count|
-		puts "%d: %s" % [revs.length - (count + 1), revs[count]]
-	end
-else
-	revs.reverse!
-	revno = ARGV.shift
-
-	if revno =~ /\.\.\./ then
-		first, last = revno.split('...').map {|a| a.to_i}
-		puts "%s...%s" % [revs[first], (last) ? revs[last] : ""]
-	elsif revno =~ /\.\./ then
-		first, last = revno.split('..').map {|a| a.to_i}
-		puts "%s..%s" % [revs[first], (last) ? revs[last] : ""]
+def parse_revision rev
+	if rev == "0"
+		0
+	elsif rev.to_i != 0 and rev !~ /[\._]/ then
+		rev.to_i
 	else
-		# but if there's only one...
-		revno = revno.to_i
-		puts revs[revno]
+		rev
 	end
 end
 
+if __FILE__ == $0
+	revs = `git log --pretty=oneline`.split("\n").map{|commit| commit.split[0]}
+
+	if (ARGV.length == 0) then
+		revs.length.times do |count|
+			puts "%d: %s" % [revs.length - (count + 1), revs[count]]
+		end
+	else
+		revs.reverse!
+		revno = ARGV.shift
+
+		# check for ranges
+		if revno =~ /\.\.\./ then
+			first, last = revno.split('...').map {|rev| parse_revision rev}
+			puts "%s..%s" % [(first.is_a? Fixnum) ? revs[first] : first,
+				             (last != nil) ? ((last.is_a? Fixnum) ? revs[last] \
+				                                                  : last) \
+				                           : ""]
+		elsif revno =~ /\.\./ then
+			first, last = revno.split('..').map {|rev| parse_revision rev}
+			puts "%s..%s" % [(first.is_a? Fixnum) ? revs[first] : first,
+				             (last != nil) ? ((last.is_a? Fixnum) ? revs[last] \
+				                                                  : last) \
+				                           : ""]
+		else
+			# but if there's only one...
+			revno = parse_revision revno
+			if revno.is_a? Fixnum then
+				puts revs[revno]
+			else
+				puts revno
+			end
+		end
+	end
+end
