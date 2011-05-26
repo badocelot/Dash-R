ec69adf588c15d437769b626782ea3504a0203d8
James M. Jensen II <badocelot@badocelot.com>
2011-05-25 20:27:16 -0500

    Added a few new features, and more verbose license statement.
      1. Usage information with --help
    
      2. Get number of commits in branch count with --count
    
      3. Silently ignore out-of-bounds revision numbers.

--- dash-r.rb
+++ dash-r.rb
@@ -1,8 +1,24 @@
 #!/usr/bin/env ruby
 
-# -r -- git revisions numbers at last!
+# Dash-R -- git revisions numbers at last!
 # Copyright 2011  James M. Jensen II <badocelot@badocelot.com>
-# License: GNU General Public License, Version 2 or later
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+# Format: [major, minor, bugfix, status]
+DASH_R_VERSION = [0,9,0,'']
 
 def parse_revision rev
 	if rev == "0"
@@ -14,38 +30,84 @@ def parse_revision rev
 	end
 end
 
-if __FILE__ == $0
+def commit_count
+	`git log --pretty=format:'' | wc -l`.to_i + 1
+end
+
+if __FILE__ == $0 then
 	if (ARGV.length == 0) then
-		# print the log, w/ rev #'s
+		# Print the log, w/ rev numbers
+		
+		# Grab the normal log from git
 		log = `git log`
+		
 		# Escape existing %'s
 		log.gsub!('%','%%')
+		
 		# Replace the "commit <hash>" lines with something more useful...
-		log.gsub!(/^(commit) ([0-9a-fA-F]+)$/,
+		log.gsub!(/^(commit) ([0-9a-fA-F]{40})$/,
 		          "\\1 %d  id: \\2")
 		
-		# print the whole mess, putting version numbers in there
-		# TODO find a better way of getting the commits count
-		num_revs = `git log --pretty=format:'' | wc -l`.to_i + 1
-		puts log % num_revs.times.map.reverse
+		# Print the new log, with commit numbers, counting from zero
+		#
+		# Commit lines should now appear like:
+		#
+		#   commit 0  id: c5b1538a56654c096472031f1195720b18f88a4f
+		#
+		puts log % (0...commit_count).to_a.reverse
+		
+	elsif ARGV.member? "--help" then
+		# Output usage information
+		
+		# Find the name under which this was invoked
+		c = $0.split('/')[-1]
+		
+		usage = [
+		"Dash-R v%d.%d.%d%s" % DASH_R_VERSION,
+		"Usage: #{c}                  :: Print git log with revision numbers",
+		"       #{c} <revno> [...]    :: Output commit(s) or range of commits",
+		"                                - Accepts .. and ... for ranges)",
+		"                                - Accepts negative numbers ala bzr/hg",
+		"",
+		"       #{c} --help           :: Show this message",
+		"       #{c} --count          :: Display the number of commits",
+		"",
+		"`#{c} <revno>` can be used to introduce git to revision numbers:",
+		"",
+		"    git diff `#{c} 2..-2` ==> git diff <arcane hash>..HEAD^",
+		"",
+		"Non-integer tags will be left unparsed:",
+		"",
+		"    v0.2..-2 ==> v0.2..924b23bee3e67de575c20c70c7a89ddddb2b5c30",
+		"",
+		"Invalid revision numbers will be silently ignored.",
+		""
+		]
+		
+		# TODO: clean this up
+		puts usage
+	elsif ARGV.member? "--count"
+		# Print the number of commits in the log
+		puts commit_count
 	else
 		# get the checksums
 		revs = `git log --pretty=format:'%H'`.split("\n").reverse
-		revno = ARGV.shift
 
-		# check for ranges
-		if revno =~ /[^\.](\.{2,3})[^\.]/ then
-			dots = $1
-			first, last = revno.split(dots).map {|rev| parse_revision rev}
-			puts ((first.is_a? Fixnum) ? revs[first] : first) + dots \
-			     + ((last) ? ((last.is_a? Fixnum) ? revs[last] : last) : "")
-		else
-			# but if there's only one...
-			revno = parse_revision revno
-			if revno.is_a? Fixnum then
-				puts revs[revno]
+		ARGV.each do |revno|
+			# check for ranges
+			if revno =~ /[^\.](\.{2,3})[^\.]/ then
+				dots = $1
+				first, last = revno.split(dots).map {|rev| parse_revision rev}
+				puts ((first.is_a? Fixnum) ? revs[first] : first) + dots \
+				     + ((last) ? ((last.is_a? Fixnum) ? revs[last] : last) : "")
 			else
-				puts revno
+				# but if there's only one...
+				revno = parse_revision revno
+				if revno.is_a? Fixnum then
+					puts revs[revno] if revs[revno]
+				else
+					puts revno
+				end
 			end
 		end
 	end
