a28e4bc4386a0b53d70cfa6705c98f263a86511d
James M. Jensen II <badocelot@badocelot.com>
2012-03-09 20:15:42 -0600

    Bumped version to 1.0.0rc2.

--- dash-r.pl
+++ dash-r.pl
@@ -22,7 +22,7 @@ use warnings;
 use Text::ParseWords;
 
 # Format: [major, minor, bugfix, status]
-my @DASH_R_VERSION = (1,0,0,'rc1');
+my @DASH_R_VERSION = (1,0,0,'rc2');
 
 sub commitCount {
    open COUNT, q(git log --pretty=format:''|);
